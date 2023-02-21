547abed49e736b0e3a753bb3adb3219328f12204
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-02-21 10:14:28 -0300

    fix(server): fix root server domain access

--- src/app.js
+++ src/app.js
@@ -7,14 +7,13 @@ const server = express();
 
 server.use(express.json());
 server.use(morgan('dev'));
-// server.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'https://pi-flame.vercel.app'); // update to match the domain you will make the request from
-//   // res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); 
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//   next();
-// });
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5173, https://portfolio-fabian.up.railway.app'); 
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 server.use('/', routes);
 
