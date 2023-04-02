fd981d80ae3c365869f3937f009a078c53e0cff7
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-04-02 10:55:45 -0300

    fix(root): change config db <alter:true>

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const {
 } = process.env;
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ alter: true }).then(() => {
   server.listen(PORT, () => {
     console.log('%s listening at', PORT); // eslint-disable-line no-console
   });
